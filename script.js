b63ee69c3c6e2c1fa789c5e6e1826819ef776094
Maciej Niemiec <niemiec.maciej@gmail.com>
2021-07-14 22:37:21 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-const randomNumber = (closingNumber) => {
+const generatorButton = document.querySelector(".generator")
+
+const resetButton = document.querySelector(".reset")
+resetButton.addEventListener("click", () => {
+        reset()
+});
+
+function randomNumber(closingNumber) {
     return Math.floor((Math.random() * closingNumber) + 1)
 };
 
-
-const insertNumber = (closingNumber) => {
-
-    const generatorButton = document.querySelector(".generator")
+function insertNumber() {
     const numbers = [];
 
     generatorButton.addEventListener("click",  click = () => {
-
-        const number = randomNumber(closingNumber);
+        const number = randomNumber(49);
 
         if (numbers.indexOf(number) === -1 && numbers.length < 6) {
-
             numbers.push(number)
 
             if (numbers[5] !== undefined) {
-
-                document.querySelector(".reset")
-                .classList.remove("hidden")
+                resetButton.classList.remove("hidden")
 
                 } else {
-
-                const resetButton = document.querySelector(".reset")
                 resetButton.addEventListener("click", () => {
                     resetButton.classList.add("hidden")
                     });
@@ -33,24 +31,17 @@ const insertNumber = (closingNumber) => {
             ballElement.textContent = numbers[numbers.length -1]
 
         } else if (numbers.length < 6) {
-
             click()
         };
     });
 };
 
-insertNumber(49)
+insertNumber()
 
-const reset = () => {
+function reset() {
+    const ballElements = document.querySelectorAll(".balls > div")
+    ballElements.forEach(element => element.textContent = "-" )
 
-    const resetButton = document.querySelector(".reset")
-    resetButton.addEventListener("click", () => {
-
-        const ballElement = document.querySelectorAll(".balls > div")
-        ballElement.forEach(element => element.textContent = "-" )
-
-    insertNumber(49)
-    });
+    insertNumber()
 };
 
-reset()
