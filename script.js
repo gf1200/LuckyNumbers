01ed3a52cabb3d7a98aa8d6beb750f4272b07b49
Maciej Niemiec <niemiec.maciej@gmail.com>
2021-09-28 20:28:45 +0200

    disabling and graying out buttons feature added

--- script.js
+++ script.js
@@ -13,18 +13,26 @@
     const {numbers} = data;
     const {limitNumber, amountOfBalls} = status;
 
-    const ballsContainer = '.balls';
-    const ballsElements = '.ball';
-    const numbersContainer = '.results';
-    const generatorBtn = '.generator';
-    const nextBtn = '.next';
-    const hide = 'hide';
-    const clean = '.clean'
+    const selectors = {
+        ballsContainer:'.balls',
+        ballsElements: '.ball',
+        numbersContainer: '.results',
+        generatorBtn: '.generator',
+        nextBtn: '.next',
+        clean: '.clean',
+        hide: 'hide',
+        disable: 'disabled'
+    };
+
+    const numbersContainer = document.querySelector(selectors.numbersContainer);
+    const generatorBtn = document.querySelector(selectors.generatorBtn);
+    const cleanBtn = document.querySelector(selectors.clean);
+    const nextBtn = document.querySelector(selectors.nextBtn);
 
     function init() {
-        document.querySelector(generatorBtn).addEventListener("click", generateNumbers);
-        document.querySelector(clean).addEventListener("click", cleanAll);
-        document.querySelector(nextBtn).addEventListener("click", displayNumbersList);
+        generatorBtn.addEventListener("click", generateNumbers);
+        cleanBtn.addEventListener("click", cleanAll);
+        nextBtn.addEventListener("click", displayNumbersList);
         createBallElement();
     };
 
@@ -33,21 +41,23 @@
             const element = document.createElement('div');
             element.className = `ball_${i + 1} ball`;
             element.textContent = '-';
-            document.querySelector(ballsContainer).appendChild(element);
+            document.querySelector(selectors.ballsContainer).appendChild(element);
         };
     };
 
     function generateNumbers() {
-        document.querySelector(generatorBtn).classList.add(hide);
+        generatorButton.disable();
         for (let i = 0; i < amountOfBalls; i++) {
-            setTimeout(displayBallNumber, `${(i + 1) * 600}`);
+            setTimeout(displayBallNumber, (i + 1) * 300);
         };
     };
 
     function cleanAll() {
         counter = 0;
-        document.querySelector(numbersContainer).innerHTML = '';
-        document.querySelector(generatorBtn).classList.remove(hide);
+        numbersContainer.innerHTML = '';
+        generatorButton.enable();
+        generatorButton.show();
+        buttons.hide();
         resetNumbers();
     };
 
@@ -55,7 +65,7 @@
         const number = getRandomNumber(limitNumber);
         putNumberToData(number);
         printBallNumber();
-        displayNextAndCleanBtn();
+        buttons.display();
     };
 
     function displayNumbersList() {
@@ -90,24 +100,70 @@
         document.querySelector(`.ball_${numbers.length}`).textContent = number;
     };
 
-    function displayNextAndCleanBtn() {
-        const lastBall = numbers.length === amountOfBalls;
-        if (lastBall) {
-            document.querySelector(nextBtn).classList.remove(hide);
-            document.querySelector(clean).classList.remove(hide);
-        };
-    };
-
     function putNumbersIntoList(itemNumber) {
         element = `<div class="numbers"><span class="counter">${itemNumber}:</span>${numbers.join('  .  ')}</div>`;
-        document.querySelector(numbersContainer).insertAdjacentHTML('afterbegin', element);
+        numbersContainer.insertAdjacentHTML('afterbegin', element);
     };
 
     function resetNumbers() {
-        document.querySelectorAll(ballsElements).forEach(element => element.textContent = "-" );
-        document.querySelector(nextBtn).classList.add(hide);
-        document.querySelector(clean).classList.add(hide);
+        document.querySelectorAll(selectors.ballsElements).forEach(element => element.textContent = "-" );
         numbers.length = 0;
+        buttons.disable();
     };
-}
+
+    const generatorButton = {
+        disable() {
+            generatorBtn.classList.add(selectors.disable);
+            generatorBtn.setAttribute('disabled', '');
+        },
+
+        enable() {
+            generatorBtn.classList.remove(selectors.disable);
+            generatorBtn.removeAttribute('disabled', '');
+        },
+
+        hide() {
+            generatorBtn.classList.add(selectors.hide);
+        },
+
+        show() {
+            generatorBtn.classList.remove(selectors.hide);
+        }
+    };
+
+    const buttons = {
+        disable() {
+            nextBtn.classList.add(selectors.disable);
+            nextBtn.setAttribute('disabled', '');
+            cleanBtn.classList.add(selectors.disable);
+            cleanBtn.setAttribute('disabled', '');
+        },
+
+        enable() {
+            nextBtn.classList.remove(selectors.disable);
+            nextBtn.removeAttribute('disabled', '');
+            cleanBtn.classList.remove(selectors.disable);
+            cleanBtn.removeAttribute('disabled', '');
+        },
+
+        hide() {
+            nextBtn.classList.add(selectors.hide);
+            cleanBtn.classList.add(selectors.hide);
+        },
+
+        show() {
+            nextBtn.classList.remove(selectors.hide);
+            cleanBtn.classList.remove(selectors.hide);
+        },
+
+        display() {
+            const lastBall = numbers.length === amountOfBalls;
+            if (lastBall) {
+                generatorButton.hide();
+                buttons.enable();
+                buttons.show();
+            };
+        }
+    };
+};
 init();
