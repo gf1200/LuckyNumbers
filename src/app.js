df51c49ec115cf7870357f48c2e4d1fd6314cf12
Wojciech Niemiec <Wojciech.Niemiec@ringieraxelspringer.pl>
2021-11-29 08:38:21 +0100

    Init react

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+import ReactDOM from "react-dom";
+
+function App() {
+    return <>
+        <div class="container">
+            <h1 class="title">Lucky Lottery Numbers</h1>
+            <div class="balls">
+            </div>
+            <div class="drowning">
+                <button class="generator">Generate numbers</button>
+                <button class="generator next hide">Next</button>
+            </div>
+            <div class="results">
+            </div>
+        </div>
+    </>
+}
+
+const app = document.getElementById("app");
+ReactDOM.render(<App />, app);
